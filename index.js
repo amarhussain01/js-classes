e0227808aff462b396585669de64e95a7e3dbad6
Amar Hussain <99237742+amarhussain01@users.noreply.github.com>
2023-11-10 21:28:42 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+// class = a blueprint for creating objects
+//         define what properties and methods they have
+//         use a constructor for unique properties
+
+class player {
+
+    score = 0;
+
+    pause(){
+        console.log("You paused the game");
+    }
+    exit(){
+        console.log("You exited the game");
+    }
+}
+
+const player1 = new player();
+const player2 = new player();
+const player3 = new player();
+const player4 = new player();
+
+player1.score += 1;
+
+console.log(player1.score);
+console.log(player2.score);
+
+player1.pause();
+player2.exit();
